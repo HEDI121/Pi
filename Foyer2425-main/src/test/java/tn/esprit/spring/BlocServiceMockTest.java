7a6cdeefea6663c264795ad734a8160756b1f1dd
HEDI121 <arramihedi50@gmail.com>
2025-07-16 16:01:30 +0100

    add

--- Foyer2425-main/src/test/java/tn/esprit/spring/BlocServiceMockTest.java
+++ Foyer2425-main/src/test/java/tn/esprit/spring/BlocServiceMockTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class BlocServiceMockTest {
 
