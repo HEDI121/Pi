d505ad8e01cdb52b85f6c9ec1248f087cdd44c00
HEDI121 <arramihedi50@gmail.com>
2025-07-16 16:24:04 +0100

    haya

--- Foyer2425-main/src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ Foyer2425-main/src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -3,10 +3,12 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // <<== Ajouté
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class) // <<== Correction ici aussi
 public class BlocServiceTest {
